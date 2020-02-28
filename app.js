cb86b2aac0811451e8d21a1e9d46220e82dc2c8b
Lyashka <iuliana9864@gmail.com>
2020-02-28 10:21:23 +0300

    add infomation first day

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
+const buttonDay1Element = document.querySelector(".day1");
+const buttonDay2Element = document.querySelector(".day2");
+const buttonDay3Element = document.querySelector(".day3");
+const buttonDay4Element = document.querySelector(".day4");
+const buttonDay5Element = document.querySelector(".day5");
 
-const weather = {};
+const day1 = {}; const day2 = {}; const day3 = {}; const day4 = {}; const day5 = {};
+const uniqDays = [];
+const mass = [];
 
-weather.temperature = {
-    unit : "celsius"
+day1.temperature = {
+  unit : "celsius"
+}
+day2.temperature = {
+  unit : "celsius"
+}
+day3.temperature = {
+  unit : "celsius"
+}
+day4.temperature = {
+  unit : "celsius"
+}
+day5.temperature = {
+  unit : "celsius"
 }
 
 const KELVIN = 273;
@@ -33,7 +52,7 @@ function showError(error){
 }
 
 function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     console.log(api);
     
@@ -43,11 +62,12 @@ function getWeather(latitude, longitude){
             return data;
         })
         .then(function(data){
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
+            day1.name = data.list[0].dt_txt.slice(0, -9).split("-").reverse().join(".");
+            day1.temperature.value = Math.floor(data.list[0].main.temp - KELVIN);
+            day1.description = data.list[0].weather[0].description;
+            day1.iconId = data.list[0].weather[0].icon;
+            day1.city = data.city.name;
+            day1.country = data.city.country;
         })
         .then(function(){
             displayWeather();
@@ -55,12 +75,23 @@ function getWeather(latitude, longitude){
 }
 
 function displayWeather(){
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+  buttonDay1Element.innerHTML = `<button><p>${day1.name}</p></button>`;
+  iconElement.innerHTML = `<img src="icons/${day1.iconId}.png"/>`;
+  tempElement.innerHTML = `${day1.temperature.value}°<span>C</span>`;
+  descElement.innerHTML = day1.description;
+  locationElement.innerHTML = `${day1.city}, ${day1.country}`;
 }
+/*
+function filteredUniqDays(mass){
+  const filteredDay = mass.filter((day) => {
+    let currentDate = day.dt_txt.slice(0, -9).split("-").reverse().join(".");
+    if (!uniqDates.includes(currentDate)) {
+      uniqDates = [...uniqDates, ...[currentDate]];
+    }
 
+    return uniqDays;
+}*/
+/*
 function celsiusToFahrenheit(temperature){
     return (temperature * 9/5) + 32;
 }
@@ -78,5 +109,5 @@ tempElement.addEventListener("click", function(){
         tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
         weather.temperature.unit = "celsius"
     }
-});
+});*/
 
